98357a653c763c3303f6bf86e69055d95c01c995
Sai-Akshaya-L <144267027+Sai-Akshaya-L@users.noreply.github.com>
2023-09-06 23:05:21 +0530

    Delete naan mudlavan/main.py

--- naan mudlavan/main.py
+++
@@ -1,9 +0,0 @@
-#factorial
-def fact(n):
-  if n==0 or n==1:
-    return 1
-  else:
-     return n*fact(n-1)
-number=int(input("Enter the number:"))
-recur=fact(number)
-print("The factorial of",number,"is:",recur)
