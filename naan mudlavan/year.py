9c326318ebbf4c031c559e098ea66cc4bf0ccda4
Sai-Akshaya-L <144267027+Sai-Akshaya-L@users.noreply.github.com>
2023-09-06 22:54:06 +0530

    Add files via upload

---
+++ naan mudlavan/year.py
@@ -0,0 +1,12 @@
+#leapyear
+def isleap(y):
+  if((year%4==0 and year%100==0) or year%400==0):
+    return True
+  else:
+    return False
+year=int(input("Enter a year:"))
+if isleap(year):
+  print(year," is a leap year")
+else:
+  print(year," is not a leap year")
+  
